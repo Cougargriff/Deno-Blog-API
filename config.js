f342ce4b0c75e18c6cd64dd7147fc5f5e7b76ae7
Griffin Johnson <cougargriff@gmail.com>
2020-08-18 23:32:41 -0700

    env works

--- config.js
+++ config.js
@@ -1,2 +1,5 @@
-export const APP_HOST = Deno.env.get("APP_HOST") || "127.0.0.1";
-export const APP_PORT = Deno.env.get("APP_PORT") || 4000;
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+const env = config()
+
+export const APP_HOST = env.APP_HOST || "127.0.0.1";
+export const APP_PORT = env.APP_PORT || 4000;
