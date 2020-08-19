df8c9012750697595872c5fcc6f529221fc95482
Griffin Johnson <cougargriff@gmail.com>
2020-08-19 15:47:47 -0700

    all env works

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 const env = config()
+console.log(Deno.env)
 
 export const APP_HOST = env.APP_HOST || "127.0.0.1";
 export const APP_PORT = env.APP_PORT || 4000;
 export const PSW = env.PSW || undefined
 export const DB_NAME = env.DB_NAME || undefined
-export const DB_PORT = env.DB_PORT || undefined
+export const DB_PORT = parseInt(env.DB_PORT) || undefined
 export const DB_USER = env.DB_USER || undefined
+export const DB_HOST = env.DB_HOST || undefined
--- db/database.js
+++ db/database.js
@@ -1,17 +1,31 @@
 import { Client } from "https://deno.land/x/postgres/mod.ts";
+import {
+  APP_HOST,
+  APP_PORT,
+  PSW,
+  DB_NAME,
+  DB_HOST,
+  DB_PORT,
+  DB_USER
+} from "../config.js";
 
 class Database {
   constructor() {
     this.connect();
+    console.log("DB_PORT ", typeof(DB_PORT))
+    console.log('DB_HOST',  DB_HOST)
+    console.log("DB_USER ", DB_USER)
+    console.log("DB_NAME ", DB_NAME)
+    console.log("PSW", PSW)
   }
 
   async connect() {
     this.client = new Client({
-      user: "postgres",
-      database: "deno_blog",
-      hostname: "localhost",
-      password: "dogs2008",
-      port: 5432
+      user: DB_USER,
+      database: DB_NAME,
+      hostname: DB_HOST,
+      password: PSW,
+      port: DB_PORT
     })
     
     await this.client.connect()
