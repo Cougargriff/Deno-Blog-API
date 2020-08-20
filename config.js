bc4c34d6087943dc1b753a15ed82592aa4487c6d
Griffin Johnson <cougargriff@gmail.com>
2020-08-19 17:22:23 -0700

    init

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 const env = config()
-console.log(Deno.env)
 
 export const APP_HOST = env.APP_HOST || "127.0.0.1";
 export const APP_PORT = env.APP_PORT || 4000;
