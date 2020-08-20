b4625328bf925f48f21cbf27ce8cbbb3cc0d4e55
Griffin Johnson <cougargriff@gmail.com>
2020-08-20 09:50:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ app.use(router.routes());
 app.use(router.allowedMethods());
 app.use(_404);
 
-console.log(`Listening on port:${APP_PORT}...`);
+console.log(`Listening on port: ${APP_PORT}...`);
 
-await app.listen(`${APP_HOST}:${APP_PORT}`);
+await app.listen(`${Deno.env.get("HOST") || "127.0.0.1"}:${Deno.env.get("PORT") || 4000}`);
